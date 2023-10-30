7b3b1fed64bfcedfe46dddbc33d75475a0933d34
pvw-primal <pvw3111@gmail.com>
2023-10-30 11:37:36 -0400

    Removed camera functionality

--- Scripts/Entities/Player/CameraControl.gd
+++ Scripts/Entities/Player/CameraControl.gd
@@ -1,13 +1,13 @@
 extends Camera3D
 const SPEED : float = 50
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_key_pressed(KEY_I):
-		position.z += SPEED * delta
-	if Input.is_key_pressed(KEY_K):
-		position.z -= SPEED * delta
-	if Input.is_key_pressed(KEY_J):
-		position.x += SPEED * delta
-	if Input.is_key_pressed(KEY_L):
-		position.x -= SPEED * delta
+
+#func _process(delta):
+#	if Input.is_key_pressed(KEY_I):
+#		position.z += SPEED * delta
+#	if Input.is_key_pressed(KEY_K):
+#		position.z -= SPEED * delta
+#	if Input.is_key_pressed(KEY_J):
+#		position.x += SPEED * delta
+#	if Input.is_key_pressed(KEY_L):
+#		position.x -= SPEED * delta
 	
